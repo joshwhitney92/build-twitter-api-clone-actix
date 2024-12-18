eb09cbb4f0aceda29c7020ec56b03040ebecd1f0
josh <joshwhtny@yahoo.com>
2024-12-17 16:17:56 -0800

    cargo fmt

--- section_3/twitter-api/src/common_tests/actix_fixture.rs
+++ section_3/twitter-api/src/common_tests/actix_fixture.rs
@@ -1,25 +1,31 @@
 use crate::{
-    common::{ app_state::AppState, fs::file_utils::get_avatar_buffer, entities::{base::DbRepo}},
+    common::{app_state::AppState, entities::base::DbRepo, fs::file_utils::get_avatar_buffer},
     routes::{
-        profiles::{ profile_route::{ create_profile, get_profile, get_profile_by_user } }, messages::message_route::{create_message, get_message, get_messages},
+        messages::message_route::{create_message, get_message, get_messages},
+        profiles::profile_route::{create_profile, get_profile, get_profile_by_user},
     },
 };
-use chrono::{ DateTime, Utc };
-use serde::Deserialize;
-use sqlx::{ FromRow };
-use actix_web::{ App, web::{ self, BytesMut, Bytes }, Error, test, dev::{ Service, ServiceResponse } };
 use actix_http::Request;
-use fake::{
-    Fake,
-    faker::{
-        internet::{ en::Username, en::DomainSuffix },
-        name::en::{ FirstName, LastName },
-        address::en::CountryName,
-    },
+use actix_web::{
+    dev::{Service, ServiceResponse},
+    test,
+    web::{self, Bytes, BytesMut},
+    App, Error,
 };
-use fake::faker::lorem::en::Sentence;
+use chrono::{DateTime, Utc};
 use fake::faker::company::en::CompanyName;
-use std::{ ops::Range };
+use fake::faker::lorem::en::Sentence;
+use fake::{
+    faker::{
+        address::en::CountryName,
+        internet::{en::DomainSuffix, en::Username},
+        name::en::{FirstName, LastName},
+    },
+    Fake,
+};
+use serde::Deserialize;
+use sqlx::FromRow;
+use std::ops::Range;
 
 pub const PUBLIC_GROUP_TYPE: i32 = 1;
 pub const CIRCLE_GROUP_TYPE: i32 = 2;
@@ -86,18 +92,20 @@ pub async fn get_app_data<T>(db_repo: T) -> web::Data<AppState<T>> {
 pub async fn get_app() -> impl Service<Request, Response = ServiceResponse, Error = Error> {
     let app_data = get_app_data(DbRepo::init().await).await;
     test::init_service(
-        App::new()
-            .app_data(app_data.clone())            
-            .service(
-                web::scope("/v1")
-                    .service(web::resource("/msg/{id}").route(web::get().to(get_message::<DbRepo>)))
-                    .service(web::resource("/msg").route(web::post().to(create_message::<DbRepo>)))
-                    .service(web::resource("/msgs").route(web::post().to(get_messages::<DbRepo>)))
-                    .service(web::resource("/profile/{id}").route(web::get().to(get_profile::<DbRepo>)))
-                    .service(web::resource("/profile/username/{user_name}").route(web::get().to(get_profile_by_user::<DbRepo>)))
-                    .service(web::resource("/profile").route(web::post().to(create_profile::<DbRepo>)))
-            )
-    ).await
+        App::new().app_data(app_data.clone()).service(
+            web::scope("/v1")
+                .service(web::resource("/msg/{id}").route(web::get().to(get_message::<DbRepo>)))
+                .service(web::resource("/msg").route(web::post().to(create_message::<DbRepo>)))
+                .service(web::resource("/msgs").route(web::post().to(get_messages::<DbRepo>)))
+                .service(web::resource("/profile/{id}").route(web::get().to(get_profile::<DbRepo>)))
+                .service(
+                    web::resource("/profile/username/{user_name}")
+                        .route(web::get().to(get_profile_by_user::<DbRepo>)),
+                )
+                .service(web::resource("/profile").route(web::post().to(create_profile::<DbRepo>))),
+        ),
+    )
+    .await
 }
 
 pub fn get_fake_message_body(prefix: Option<String>) -> String {
@@ -113,49 +121,57 @@ pub fn get_fake_message_body(prefix: Option<String>) -> String {
     body
 }
 
+// NOTE: Creating a multipart form for testing purposes.
 /// warning: line breaks are very important when ending any line!!!
 pub fn get_profile_create_multipart(
     avatar: &Vec<u8>,
     boundary: &str,
-    with_avatar: bool
+    with_avatar: bool,
 ) -> BytesMut {
     let mut payload = actix_web::web::BytesMut::new();
     payload.extend(format!("--{}\r\n", boundary).as_bytes());
-    payload.extend(
-        format!("Content-Disposition: form-data; name=\"user_name\"\r\n\r\n").as_bytes()
-    );
+    payload
+        .extend(format!("Content-Disposition: form-data; name=\"user_name\"\r\n\r\n").as_bytes());
     payload.extend(format!("{}\r\n", Username().fake::<String>()).as_bytes());
     payload.extend(format!("--{}\r\n", boundary).as_bytes());
+    payload
+        .extend(format!("Content-Disposition: form-data; name=\"full_name\"\r\n\r\n").as_bytes());
     payload.extend(
-        format!("Content-Disposition: form-data; name=\"full_name\"\r\n\r\n").as_bytes()
-    );
-    payload.extend(
-        format!("{} {}\r\n", FirstName().fake::<String>(), LastName().fake::<String>()).as_bytes()
+        format!(
+            "{} {}\r\n",
+            FirstName().fake::<String>(),
+            LastName().fake::<String>()
+        )
+        .as_bytes(),
     );
     payload.extend(format!("--{}\r\n", boundary).as_bytes());
+    payload
+        .extend(format!("Content-Disposition: form-data; name=\"description\"\r\n\r\n").as_bytes());
     payload.extend(
-        format!("Content-Disposition: form-data; name=\"description\"\r\n\r\n").as_bytes()
-    );
-    payload.extend(
-        format!("{}\r\n", Sentence(Range { start: 8, end: 10 }).fake::<String>()).as_bytes()
+        format!(
+            "{}\r\n",
+            Sentence(Range { start: 8, end: 10 }).fake::<String>()
+        )
+        .as_bytes(),
     );
     payload.extend(format!("--{}\r\n", boundary).as_bytes());
     payload.extend(format!("Content-Disposition: form-data; name=\"region\"\r\n\r\n").as_bytes());
     payload.extend(format!("{}\r\n", CountryName().fake::<String>()).as_bytes());
     payload.extend(format!("--{}\r\n", boundary).as_bytes());
-    
-    payload.extend(format!("Content-Disposition: form-data; name=\"main_url\"\r\n\r\n").as_bytes());    
+
+    payload.extend(format!("Content-Disposition: form-data; name=\"main_url\"\r\n\r\n").as_bytes());
     payload.extend(get_fake_main_url().as_bytes());
-    payload.extend(b"\r\n"); // warning: line breaks are very important!!! 
+    payload.extend(b"\r\n"); // warning: line breaks are very important!!!
     payload.extend(format!("--{}\r\n", boundary).as_bytes());
 
     if with_avatar == true {
         payload.extend(
-            b"Content-Disposition: form-data; name=\"avatar\"; filename=\"profile.jpeg\"\r\n"
+            // NOTE: Putting `b` in front of a string will is the same as calling `.as_bytes`! - JW
+            b"Content-Disposition: form-data; name=\"avatar\"; filename=\"profile.jpeg\"\r\n",
         );
         payload.extend(b"Content-Type: image/jpeg\r\n\r\n");
         payload.extend(Bytes::from(avatar.clone()));
-        payload.extend(b"\r\n"); // warning: line breaks are very important!!!        
+        payload.extend(b"\r\n"); // warning: line breaks are very important!!!
     }
     payload.extend(format!("--{}--\r\n", boundary).as_bytes()); // note the extra -- at the end of the boundary
 
